c5a97968ba12591ca39ee603f423793a9b2c0785
Alim-ziedinov <a-zidan@rambler.ru>
2022-11-16 11:04:06 +0300

    commit variables1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,18 +2,17 @@ function solveEquation(a, b, c) {
   let arr = [];
   let discriminant = Math.pow(b, 2) - 4 * a * c;
   if(discriminant < 0){
-    let arr = 0; 
+    let arr = []; 
     return(arr);
-  }else if(discriminant == 0){
+  }
+  else if(discriminant == 0){
     let root = -b / (2 * a);
     arr[0] = Number(root.toFixed(2));
-    return(arr)
-  }else if(discriminant > 0){
+  }
+   else if(discriminant > 0){
     let root1 = (-b + Math.sqrt(discriminant) )/(2*a);
     let root2 = (-b - Math.sqrt(discriminant) )/(2*a);
-    arr[0] = Number(root1.toFixed(2));
-    arr[1] = Number(root2.toFixed(2));
-    return( arr);
+    arr = [root1, root2];
   }
    return arr; // array
 }
